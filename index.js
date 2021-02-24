8404d1d5406eb6be6512bda0e7ad6592bb7cb02a
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-23 19:29:09 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+// saturday fun function. what do i want to do on saturday?
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(adjective = "special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
